44560149f87648bacdfbd67fdefd602929e85d73
luoyunchong <luoyunchong@foxmail.com>
2020-04-21 11:34:45 +0800

    remove 文档

--- README.md
+++ README.md
@@ -5,10 +5,7 @@
 完整文档请点击此链接 
 - [https://luoyunchong.github.io/vuepress-docs/](https://luoyunchong.github.io/vuepress-docs)
 
-
-主要分为三部分：
 - lin-cms  [https://luoyunchong.github.io/vuepress-docs/dotnetcore/lin-cms/](https://luoyunchong.github.io/vuepress-docs/dotnetcore/lin-cms/) 
-- dotnetcore-examples  [https://luoyunchong.github.io/vuepress-docs/dotnetcore/examples/](https://luoyunchong.github.io/vuepress-docs/dotnetcore/examples/) 
 - colorui [https://luoyunchong.github.io/vuepress-docs/colorui/docs/](https://luoyunchong.github.io/vuepress-docs/colorui/docs/)
 
 前提
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,7 @@ module.exports = {
     // 文档仓库
     docsRepo: 'https://github.com/luoyunchong/vuepress-docs',
     // 假如文档不是放在仓库的根目录下：
-    docsDir: 'docs',
+    docsDir: 'vuepress-docs',
     // 假如文档放在一个特定的分支下：
     docsBranch: 'master',
     // 默认是 false, 设置为 true 来启用
@@ -29,7 +29,6 @@ module.exports = {
     nav: [
       { text: '简介', link: '/' },
       { text: 'lin-cms-dotnetcore起步', link: '/dotnetcore/lin-cms/' },
-      { text: 'dotnetcore指南', link: '/dotnetcore/examples/' },
       { text: 'ColorUI指南', link: '/colorui/docs/' },
       { text: '关于我', link: 'http://blog.igeekfan.cn/about/' },
       {
@@ -37,30 +36,12 @@ module.exports = {
         link: 'https://github.com/luoyunchong'
       }],
     sidebar: {
-      '/dotnetcore/examples/': [
-        {
-          title: 'Examples',
-          collapsable: true,
-          children: [
-            'Console-Hello-World',
-            'Console-News-Types',
-            'FreeSql-in-asp.net-core-webapi-how-to-use',
-            'FreeSql-sample-blog-RESTful-use-automapper',
-            'IdentityServer4',
-            'Qiniu-Object-Storage',
-            'ImCore-Chat'
-          ]
-        }
-      ],
       '/dotnetcore/lin-cms/': [{
-        title: 'lin-cms-dotnetcore起步',
+        title: 'lin-cms-dotnetcore',
         collapsable: true,
         children: [
           'dotnetcore-start.md',
           'vue-start.md',
-          'Open-source-road.md',
-          'Module-Design.md',
-          'Production-Design.md',
           'pm-design-modules.md',
         ]
       }, {
@@ -68,13 +49,8 @@ module.exports = {
         collapsable: true,
         children: [
           'dev-start',
-          'Newtonsoft.Json-question',
           'dependency-injection',
           'dynamic-authorization-in-asp-net-core',
-          'Reflex-Assembly-Get-Controller-Methods-Attribute',
-          'IdentityServer4-JWT',
-          'ToolGood.Words',
-          'StopWords',
           'spa-github-login'
         ]
       }],
